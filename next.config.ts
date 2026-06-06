e9137568fdfcc2b35b9e52e690b8d61d01c0abed
Den Trost <dentrost@gmail.com>
2026-06-06 13:26:43 +0300

    allow external car images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cn2.pa-server.ru",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
+
+export default nextConfig;
 
 export default nextConfig;
